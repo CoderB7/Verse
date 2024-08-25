eb843f3cf4891e4f263b662c4fc24a5e2b5dc969
CoderB7 <iutstudent2022@gmail.com>
2024-08-25 17:12:28 +0500

    fix: customUser null

--- account/models.py
+++ account/models.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 from bot.models import TelegramUser
 
 class CustomUser(AbstractUser):
-    telegram_user = models.OneToOneField(TelegramUser, on_delete=models.CASCADE, related_name='django_user')
+    telegram_user = models.OneToOneField(TelegramUser, on_delete=models.CASCADE, related_name='django_user', null=True)
