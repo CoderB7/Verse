7277a8abec16996de650a793cf79ce388ade9f2a
CoderB7 <iutstudent2022@gmail.com>
2024-08-25 17:45:59 +0500

    fix: redis error

--- core/celery.py
+++ core/celery.py
@@ -4,9 +4,9 @@ from celery import Celery
 from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easy_typer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('easy_typer')
+app = Celery('verse')
 
 # Using a string here means the worker doesn’t have to serialize
 # the configuration object to child processes.
